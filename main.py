0a701867637bfe6247914c54676cc7106edc9ec2
LightMizu <1blend.your.life1@gmail.com>
2025-09-07 18:43:13 +0300

    Add icon

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import flet as ft
 from src.launcher.app import Laucher
+from pathlib import Path
 
-
+ASSETS = Path(__file__).resolve().parent.parent /"src"/"launcher"/ "assets"
+ICON_PATH = (ASSETS / "icon.ico").resolve()
 def main(page: ft.Page):
     app = Laucher(page)
     app.run()
 
 if __name__ == '__main__':
-    ft.app(target=main)
+    ft.app(target=main, assets_dir=str(ASSETS) )
