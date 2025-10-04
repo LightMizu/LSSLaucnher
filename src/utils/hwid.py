71999aaea33007b7e23c89522116ac30e573263f
1blend.your.life1@gmail.com <1blend.your.life1@gmail.com>
2025-10-04 09:29:32 +0300

    Fix hwid

--- src/utils/hwid.py
+++ src/utils/hwid.py
@@ -6,15 +6,22 @@ def get_hwid(salt: str = '') -> str:
     """
     Возвращает кроссплатформенный уникальный идентификатор машины (HWID)
     """
-
-
-    # На Windows можно добавить VolSer или DiskSerial
+    
+    uuid_str = None
     if platform.system() == "Windows":
         import wmi
+        
         try:
             uuid_str = wmi.WMI().Win32_ComputerSystemProduct()[0].UUID
         except:
             pass
+        if not uuid_str:
+            try:
+                command = 'powershell -Command "(Get-CimInstance -ClassName Win32_ComputerSystemProduct).UUID"'  # noqa: E501
+                output = subprocess.check_output(command, shell=True, text=True)
+                uuid_str = output.strip()
+            except:
+                pass
     else:
         # На Linux/Mac можно использовать disk UUID
         try:
@@ -26,7 +33,8 @@ def get_hwid(salt: str = '') -> str:
                 uuid_str = output.decode().split('=')[1].strip().strip('"')
         except:
             pass
-
+    if not uuid_str:
+        uuid_str = "example"
     # Хешируем все данные, чтобы получить короткий HWID
     hwid = hashlib.sha256(uuid_str.encode()).hexdigest()
     return hwid
